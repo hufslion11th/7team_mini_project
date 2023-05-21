5fd3d80889b695d686d7b23794515e6d582b326d
SangYoonLee <prestro1231@hufs.ac.kr>
2023-05-22 01:18:16 +0900

    Modify: NavBar 컴포넌트 수정 완

--- react-reddit-main/client/src/components/NavBar.tsx
+++ react-reddit-main/client/src/components/NavBar.tsx
@@ -21,53 +21,71 @@ const NavBar: React.FC = () => {
   };
 
   return (
-    <div className="fixed inset-x-0 top-0 z-10 flex items-center justify-between px-5 bg-white h-13">
-      <span className="text-2xl font-semibold text-gray-400">
-        <Link href="/">
-          <a>
-            <Image
-              src="/reddit-name-logo.png"
-              alt="logo"
-              width={80}
-              height={45}
-            ></Image>
-          </a>
-        </Link>
-      </span>
-      <div className="max-w-full px-4">
-        <div className="relative flex items-center bg-gray-100 border rounded hover:border-gray-700 hover:bg-white">
-          <FaSearch className="ml-2 text-gray-400" />
-          <input
-            type="text"
-            placeholder="Search Reddit"
-            className="px-3 py-1 bg-transparent rounded h-7 focus:outline-none"
-          />
+    <div className="fixed inset-x-0 top-0 z-10 flex items-center justify-between px-5 bg-white h-12">
+      <div className="flex">
+        <span className="text-2xl font-semibold text-gray-400 p-2">
+          <Link href="/">
+            <a>
+              <Image
+                src="/images/logo.jpg"
+                alt="logo"
+                width={110}
+                height={40}
+              ></Image>
+            </a>
+          </Link>
+        </span>
+        <div className="flex justify-start items-center">
+          <span className="ml-5 mr-5 text-sm font-medium">
+            <Link href="/">아파트</Link>
+          </span>
+          <span className="ml-5 mr-5 text-sm font-medium">
+            <Link href="/">빌라, 투룸+</Link>
+          </span>
+          <span className="ml-5 mr-5 text-sm font-medium">
+            <Link href="/">원룸</Link>
+          </span>
+          <span className="ml-5 mr-5 text-sm font-medium">
+            <Link href="/">오피스텔</Link>
+          </span>
         </div>
       </div>
-
       <div className="flex">
-        {!loading &&
-          (authenticated ? (
-            <button
-              className="w-20 px-2 mr-2 text-sm text-center text-white bg-gray-400 rounded h-7"
-              onClick={handleLogout}
-            >
-              로그아웃
-            </button>
-          ) : (
-            <>
-              <Link href="/login">
-                <a className="w-20 px-2 pt-1 mr-2 text-sm text-center text-white bg-gray-400 rounded h-7">
-                  로그인
-                </a>
-              </Link>
-              <Link href="/register">
+        <div className="max-w-full px-3">
+          <div className="relative flex items-center bg-gray-100 border rounded-2xl hover:border-gray-700 hover:bg-white">
+            <FaSearch className="ml-2 text-gray-400" />
+            <input
+              type="text"
+              placeholder="Search"
+              className="px-4 py-1 bg-transparent rounded h-7 focus:outline-none"
+            />
+          </div>
+        </div>
+
+        <div className="flex">
+          {!loading &&
+            (authenticated ? (
+              <button
+                className="w-20 px-1 mr-2 text-sm text-center text-white bg-gray-400 rounded h-7"
+                onClick={handleLogout}
+              >
+                로그아웃
+              </button>
+            ) : (
+              <>
+                <Link href="/login">
+                  <a className="w-20 px-2 pt-1 mr-2 text-sm text-center text-black bg-white rounded h-7">
+                    Log In
+                  </a>
+                </Link>
+                {/* <Link href="/register">
                 <a className="w-20 px-2 pt-1 text-sm text-center text-white bg-gray-400 rounded h-7">
                   회원가입
                 </a>
-              </Link>
-            </>
-          ))}
+              </Link> */}
+              </>
+            ))}
+        </div>
       </div>
     </div>
   );
