ac92ebfdab06fab8290bd1a4471636231805137a
minsoll <sol020501@hufs.ac.kr>
2023-05-23 00:58:24 +0900

    color,logo change

--- react-reddit-main/client/src/pages/index.tsx
+++ react-reddit-main/client/src/pages/index.tsx
@@ -1,48 +1,55 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import Link from 'next/link'
-import styles from '../styles/Home.module.css'
-import useSWR from 'swr';
-import { Post, Sub } from '../types'
-import axios from 'axios'
-import { useAuthState } from '../context/auth'
-import useSWRInfinite from 'swr/infinite';
-import PostCard from '../components/PostCard'
-import { useEffect, useState } from 'react'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+import styles from "../styles/Home.module.css";
+import useSWR from "swr";
+import { Post, Sub } from "../types";
+import axios from "axios";
+import { useAuthState } from "../context/auth";
+import useSWRInfinite from "swr/infinite";
+import PostCard from "../components/PostCard";
+import { useEffect, useState } from "react";
 
 const Home: NextPage = () => {
   const { authenticated } = useAuthState();
 
   const fetcher = async (url: string) => {
-    return await axios.get(url).then(res => res.data)
-  }
+    return await axios.get(url).then((res) => res.data);
+  };
   const address = `/subs/sub/topSubs`;
 
   const getKey = (pageIndex: number, previousPageData: Post[]) => {
     if (previousPageData && !previousPageData.length) return null;
     return `/posts?page=${pageIndex}`;
-  }
+  };
 
-  const { data, error, size: page, setSize: setPage, isValidating, mutate } = useSWRInfinite<Post[]>(getKey);
+  const {
+    data,
+    error,
+    size: page,
+    setSize: setPage,
+    isValidating,
+    mutate,
+  } = useSWRInfinite<Post[]>(getKey);
   const isInitialLoading = !data && !error;
   const posts: Post[] = data ? ([] as Post[]).concat(...data) : [];
-  const { data: topSubs } = useSWR<Sub[]>(address, fetcher)
+  const { data: topSubs } = useSWR<Sub[]>(address, fetcher);
 
   const [observedPost, setObservedPost] = useState("");
 
   useEffect(() => {
-    // 포스트가 없다면 return 
+    // 포스트가 없다면 return
     if (!posts || posts.length === 0) return;
     // posts 배열안에 마지막 post에 id를 가져옵니다.
     const id = posts[posts.length - 1].identifier;
     // posts 배열에 post가 추가돼서 마지막 post가 바뀌었다면
-    // 바뀐 post 중 마지막post를 obsevedPost로 
+    // 바뀐 post 중 마지막post를 obsevedPost로
     if (id !== observedPost) {
       setObservedPost(id);
       observeElement(document.getElementById(id));
     }
-  }, [posts])
+  }, [posts]);
 
   const observeElement = (element: HTMLElement | null) => {
     if (!element) return;
@@ -57,32 +64,29 @@ const Home: NextPage = () => {
           observer.unobserve(element);
         }
       },
-      {threshold: 1}
+      { threshold: 1 }
     );
     // 대상 요소의 관찰을 시작
     observer.observe(element);
-  }
+  };
 
   return (
-    <div className='flex max-w-5xl px-4 pt-5 mx-auto'>
+    <div className="flex max-w-5xl px-4 pt-5 mx-auto">
       {/* 포스트 리스트 */}
-      <div className='w-full md:mr-3 md:w-8/12'>
-        {isInitialLoading && <p className="text-lg text-center">로딩중입니다...</p>}
-        {posts?.map(post => (
-          <PostCard
-            key={post.identifier}
-            post={post}
-            mutate={mutate}
-          />
+      <div className="w-full md:mr-3 md:w-8/12">
+        {isInitialLoading && (
+          <p className="text-lg text-center">로딩중입니다...</p>
+        )}
+        {posts?.map((post) => (
+          <PostCard key={post.identifier} post={post} mutate={mutate} />
         ))}
-
       </div>
 
       {/* 사이드바 */}
-      <div className='hidden w-4/12 ml-3 md:block'>
-        <div className='bg-white border rounded'>
-          <div className='p-4 border-b'>
-            <p className='text-lg font-semibold text-center'>상위 커뮤니티</p>
+      <div className="hidden w-4/12 ml-3 md:block">
+        <div className="bg-white border rounded-full">
+          <div className="p-4 border-b">
+            <p className="text-lg font-semibold text-center"> 커뮤니티</p>
           </div>
 
           {/* 커뮤니티 리스트 */}
@@ -104,33 +108,27 @@ const Home: NextPage = () => {
                   </a>
                 </Link>
                 <Link href={`/r/${sub.name}`}>
-                  <a className='ml-2 font-bold hover:cursor-pointer'>
+                  <a className="ml-2 font-bold hover:cursor-pointer">
                     /r/{sub.name}
                   </a>
                 </Link>
-                <p className='ml-auto font-md'>{sub.postCount}</p>
+                <p className="ml-auto font-md">{sub.postCount}</p>
               </div>
             ))}
-
-
           </div>
-          {authenticated &&
-            <div className='w-full py-6 text-center'>
+          {authenticated && (
+            <div className="w-full py-6 text-center">
               <Link href="/subs/create">
-                <a className='w-full p-2 text-center text-white bg-gray-400 rounded'>
+                <a className="w-full p-2 text-center text-white bg-gray-400 rounded">
                   커뮤니티 만들기
                 </a>
               </Link>
             </div>
-          }
+          )}
         </div>
       </div>
-
-
-
-
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
